c4eee07ead36e580d0e297df641ab5f27b5e7a9d
LuisDavid <luisdavid007@gmail.com>
2020-11-04 14:21:51 -0600

    added address variable

--- getAEMLogData.bat
+++ getAEMLogData.bat
@@ -1,7 +1,13 @@
 @ECHO OFF
 
+REM USAGE: Input the AEM log file that you want to read info from
+REM        The address variable must be modified to match the address of the logs
+REM        directory in your computer
+REM        an alternative solution is to use
+REM REQUIREMENTS: gawk (awk for windows)
+
 set fname=%1
+set address=c:\Users\luisd\Desktop\AEM\author-folder\crx-quickstart\logs\
+echo Input file is: %address%%fname%
 
-echo Input file is: %fname%
-
-REM gawk "BEGIN{print \"LOGGED CHANGES:\"} {print \"\t\" NR, $10} END {print NR \" processed\"}" rhinoDebug.log
+gawk "BEGIN{print \"LOGGED CHANGES:\"} {print \"\t\" NR, $10} END {print NR \" processed\"}" %address%%fname%
