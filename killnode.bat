f8440894827a857e5aaa8c1c3930fc2625da35dc
LuisDavid <luisdavid007@gmail.com>
2020-11-04 19:18:40 -0600

    Made cancelling operations possible

--- killnode.bat
+++ killnode.bat
@@ -7,12 +7,33 @@ REM We use awk to obtain the PID of all the processes running node
 TASKLIST | gawk "/node/{printf $2\" \"}" > temp.txt
 SET /p temp=<temp.txt
 DEL temp.txt
+ECHO Processes to kill PIDS:  %temp%
+SET /p isConfirmed="Do you want to kill these processes (Y/N): "
+goto :main
 
-IF DEFINED %temp% (
-    FOR %%x IN (%temp%) DO (
-        ECHO MESSAGE: terminating process %%x
-        TASKKILL /F /PID %%x
+:main
+    IF "%isConfirmed%"=="Y" (
+        goto :kill
+    )ELSE (
+        IF "%isConfirmed%"=="N" (
+            goto :cancel
+        )ELSE (
+            goto :cancel
+        )
     )
-) ELSE (
-    ECHO MESSAGE: No node tasks to kill
-)
+goto :eof
+
+:kill
+    IF "%temp%" NEQ "" (
+        FOR %%x IN (%temp%) DO (
+            ECHO MESSAGE: terminating process %%x
+            TASKKILL /F /PID %%x
+        )
+    ) ELSE (
+        ECHO MESSAGE: No node tasks to kill
+    )
+goto :eof
+
+:cancel
+    echo MESSAGE: No tasks killed
+goto :eof
